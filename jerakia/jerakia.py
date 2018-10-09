b4128a56d7888c281ce43ad13f8f8d61a7670210
Henning Henkel <henning@henkel.cx>
2018-10-09 09:12:59 +0200

    Reworked version with less conditionals

--- jerakia/jerakia.py
+++ jerakia/jerakia.py
@@ -1,9 +1,9 @@
 import yaml
 import os.path
+import sys
 import requests
 import json
 import msgpack
-import pprint
 
 class Error(Exception):
     """Base class for exceptions in this module."""
@@ -23,11 +23,8 @@ class JerakiaError(Error):
 
 class Jerakia(object):
     """Constructor."""
-    def __init__(self, **kwargs):
-        self.config = self.default_config()
-        for attr in ('protocol', 'host', 'port', 'version'):
-            if kwargs.get(attr) is not None:
-                self.config[attr] = kwargs.get(attr)
+    def __init__(self, protocol='http', host='localhost', port=9843, version=1):
+        self.config = dict(protocol=protocol, host=host, port=port, version=version)
         self._content_type = dict(json = 'application/json',
                 msgpack = 'application/x-msgpack')
         self.session = requests.Session()
@@ -44,54 +41,48 @@ class Jerakia(object):
         return cls(**config)
 
 
-    def default_config(self):
-        """Set the jerakia default values"""
-        return dict(
-                protocol = 'http', 
-                host = 'localhost',
-                port = 9843,
-                version = 1 
-                )
-    
-
     def get_config(self):
         """Return the jerakia config dict"""
         return self.config
 
 
-    def lookup(self, **kwargs):
-        url = '{}://{}:{}/v{}/lookup'.format(self.config['protocol'],
-                self.config['host'], self.config['port'],
-                self.config['version'])     
+    def lookup(self, namespace, token, key=None,  merge=None, lookup_type=None,
+            content_type='msgpack', policy=None, scope=None, scope_dict=None,
+            metadata_dict=None):
 
-        headers = dict()
-        headers['content-type'] = self._content_type['msgpack']
-        params = dict()
-        params['policy'] = 'default'
-        params['scope'] = 'metadata'
-        
-        for attr in ('key', 'namespace', 'policy', 'lookup_type', 'merge', 'scope',
-                'scope_dict', 'metadata_dict', 'token', 'content_type'):
-            if kwargs.get(attr) is not None:
-                if attr == 'metadata_dict':
-                    for key, val in kwargs.get(attr).items():
-                        params['metadata_{}'.format(key)] = val
-                elif attr == 'scope_dict':
-                    for key, val in kwargs.get(attr).items():
-                        params['scope_{}'.key] = val
-                elif attr == 'key':
-                    url = "{}/{}".format(url, kwargs.get(attr))
-                elif attr == 'token':
-                    headers['x-authentication'] = kwargs.get(attr)
-                elif attr == 'content_type':
-                    headers['content-type'] = self._content_type[kwargs.get(attr)]
-                else:
-                    params[attr] = kwargs.get(attr)
+        def dict_attr(dictionary, prefix):
+            target = dict()
+            for key, val in dictionary.items():
+                target['{}_{}'.format(prefix, key)] = val
+            return target
+
+        params = {k: v for k, v in (('namespace', namespace), ('merge', merge),
+            ('policy', policy), ('scope', scope), ('lookup_type', lookup_type))
+            if v is not None}
+        headers = {k: v for k, v in (('x-authentication', token),
+            ('content-type', self._content_type[content_type])) if v is not
+            None}
+
+        if metadata_dict is not None:
+            params = {**params, **dict_attr(metadata_dict, 'metadata')}
+        if scope_dict is not None:
+            params = {**params, **dict_attr(scope_dict, 'scope')}
+
+        url = '{}://{}:{}/v{}/lookup'.format(
+                self.config['protocol'],
+                self.config['host'], self.config['port'],
+                self.config['version'])
+        if key is not None:
+            url = "{}/{}".format(url, key)
+
         try:
             response = requests.get(url, params=params, headers=headers)
             response.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            raise JerakiaError("Bad HTTP response: {}".format(err))
+            print("Bad HTTP response: {}".format(err))
+            result = response.json()
+            print("Jerakia lookup {}: '{}'.".format(result['status'], result['message']))
+            sys.exit(1)
         return self._unpack_response(response)
 
 
